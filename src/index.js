a38cc4d75644dc819998b292e6204070774e23a5
rigxter <188875106+rigxter@users.noreply.github.com>
2025-04-01 19:44:06 +0200

    expand weather object to retrieve hourly data from API

--- src/index.js
+++ src/index.js
@@ -26,6 +26,26 @@ export async function getCurrentWeather(location) {
             uvindex: data.currentConditions.uvindex,
             precipProb: data.currentConditions.precipprob,
             icon: data.currentConditions.icon,
+            hourlyTemp: [
+                data.days[0].hours[0].temp,
+                data.days[0].hours[3].temp,
+                data.days[0].hours[6].temp,
+                data.days[0].hours[9].temp,
+                data.days[0].hours[12].temp,
+                data.days[0].hours[15].temp,
+                data.days[0].hours[18].temp,
+                data.days[0].hours[21].temp,
+            ],
+            hourlyTempHour: [
+                data.days[0].hours[0].datetime,
+                data.days[0].hours[3].datetime,
+                data.days[0].hours[6].datetime,
+                data.days[0].hours[9].datetime,
+                data.days[0].hours[12].datetime,
+                data.days[0].hours[15].datetime,
+                data.days[0].hours[18].datetime,
+                data.days[0].hours[21].datetime,
+            ],
         };
         return Weather;
     } catch (error) {
